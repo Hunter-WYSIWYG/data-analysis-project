34987a2b365125443e43c3ec20911193e8761d72
Hunter-WYSIWYG <hunter23798@gmail.com>
2021-08-18 13:56:11 +0200

    init constants

--- src/Scatterplot.elm
+++ src/Scatterplot.elm
@@ -1,1 +1,21 @@
 module Scatterplot exposing (..)
+
+w : Float
+w =
+    900
+
+h : Float
+h =
+    450
+
+padding : Float
+padding =
+    60
+
+radius : Float
+radius =
+    5.0
+
+tickCount : Int
+tickCount =
+    5
