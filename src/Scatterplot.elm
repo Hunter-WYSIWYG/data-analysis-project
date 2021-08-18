ecf419cde2f564ec3377f36e9b8beb2641bc1073
Hunter-WYSIWYG <hunter23798@gmail.com>
2021-08-18 14:01:58 +0200

    scale functions

--- src/Scatterplot.elm
+++ src/Scatterplot.elm
@@ -1,5 +1,39 @@
 module Scatterplot exposing (..)
 
+import Scale exposing (ContinuousScale)
+import Statistics
+
+defaultExtent : ( number, number1 )
+defaultExtent =
+    ( 0, 100 )
+
+padExtent : ( Float, Float ) -> ( Float, Float )
+padExtent ( min, max ) =
+    let
+        -- extentPadding = 0.1 * (max - min)
+
+        extentPadding = (max - min) / toFloat (tickCount * 2) 
+    in
+    ( min - extentPadding, max + extentPadding )
+
+wideExtent : List Float -> ( Float, Float )
+wideExtent values =
+    Maybe.withDefault
+        defaultExtent
+        (Maybe.map
+            padExtent
+            (Statistics.extent values)
+        )
+
+xScale : List Float -> ContinuousScale Float
+xScale values =
+    Scale.linear ( 0, w - 2 * padding ) ( wideExtent values )
+
+
+yScale : List Float -> ContinuousScale Float
+yScale values =
+    Scale.linear ( h - 2 * padding, 0 ) ( wideExtent values )
+
 w : Float
 w =
     900
