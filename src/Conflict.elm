06afe1b581d8b86a2d998c565a2ea06059e7e0d5
Hunter-WYSIWYG <hunter23798@gmail.com>
2021-08-18 13:51:17 +0200

    conflict json decoder

--- src/Conflict.elm
+++ src/Conflict.elm
@@ -1,5 +1,8 @@
 module Conflict exposing (..)
 
+import Json.Decode
+import Json.Decode.Extra
+
 type alias Conflict =
     { event_id_no_cnty : Int
     , year : Int
@@ -7,3 +10,16 @@ type alias Conflict =
     , notes : String
     , country : String
     }
+
+listDecoder : Json.Decode.Decoder a -> Json.Decode.Decoder (List a)
+listDecoder decoder =
+    Json.Decode.list decoder
+
+decodeConflict : Json.Decode.Decoder Conflict
+decodeConflict =
+    Json.Decode.succeed Conflict
+        |> Json.Decode.Extra.andMap (Json.Decode.field "EVENT_ID_NO_CNTY" Json.Decode.int)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "YEAR" Json.Decode.int)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "FATALITIES" Json.Decode.int)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "NOTES" Json.Decode.string)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "COUNTRY" Json.Decode.string)
