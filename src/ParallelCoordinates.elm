0152b3911d300ef25be08ea85222ab0f2db70f5e
Hunter-WYSIWYG <hunter23798@gmail.com>
2021-08-22 17:08:15 +0200

    pc: render parallel coordinates

--- src/ParallelCoordinates.elm
+++ src/ParallelCoordinates.elm
@@ -109,3 +109,60 @@ yScaleOrdinal valueList axisName =
                 _ -> 0
     in
     Scale.band { defaultBandConfig | paddingInner = innerPadding, paddingOuter = outerPadding, align = align } ( h - 2 * padding, 0 ) valueList
+
+parallelCoordinates : List Conflict.Conflict -> Int -> Svg msg
+parallelCoordinates conflicts year =
+    let
+        half : ( Float, Float ) -> Float
+        half t =
+            Tuple.first t + (Tuple.second t - Tuple.first t) / 2
+            
+        achsen =
+            List.indexedMap
+                (\i axisName ->
+                    case axisName of
+                        "Fatalities" ->
+                            g
+                                [ transform [ Translate (padding + ((toFloat i)*segmentDistance) - 1) (padding - 1) ]
+                                , fontSize <| Px 15.0
+                                , fontFamily [ "sans-serif" ]
+                                ]
+                                [ yAxisFloat (values axisName conflicts)
+                                , text_ [ x 0, y (-20), textAnchor AnchorMiddle ] [ Html.text axisName ]
+                                ]
+                        _ ->
+                            let
+                                scaleList =
+                                    case axisName of
+                                        "Month" -> List.range 1 12
+                                        "Event type" -> List.range 1 6
+                                        "Region" -> List.range 1 5
+                                        _ -> []
+                            in
+                            g
+                                [ transform [ Translate (padding + ((toFloat i)*segmentDistance) - 1) (padding - 1) ]
+                                , fontSize <| Px 15.0
+                                , fontFamily [ "sans-serif" ]
+                                ]
+                                [ yAxisOrdinal scaleList axisName
+                                , text_ [ x 0, y (-20), textAnchor AnchorMiddle ] [ Html.text axisName ]
+                                ]
+                )
+                dimensionNames
+
+        description =
+            g
+                [ transform [ Translate (padding) (padding) ]
+                , fontSize <| Px 15.0
+                , fontFamily [ "sans-serif" ]
+                ]
+                [ text_ [ x -20, y -40 ] [ Html.text (String.concat ["Year: ", (String.fromInt year)]) ]
+                ]
+    in
+    svg [ viewBox 0 0 w h, TypedSvg.Attributes.width <| TypedSvg.Types.Percent 100, TypedSvg.Attributes.height <| TypedSvg.Types.Percent 100 ]
+        (   achsen
+            ++
+            drawAllSegments conflicts
+            ++
+            [ description ]
+        )
