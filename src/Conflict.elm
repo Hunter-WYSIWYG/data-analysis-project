561008db821f0d75f5789ffc714e555f5e8614ed
Hunter-WYSIWYG <hunter23798@gmail.com>
2021-08-22 17:13:36 +0200

    conflicts updated

--- src/Conflict.elm
+++ src/Conflict.elm
@@ -6,9 +6,12 @@ import Json.Decode.Extra
 type alias Conflict =
     { event_id_no_cnty : Int
     , year : Int
+    , event_date : String
+    , event_type : String
     , fatalities : Int
     , notes : String
     , country : String
+    , region : String
     }
 
 listDecoder : Json.Decode.Decoder a -> Json.Decode.Decoder (List a)
@@ -20,6 +23,9 @@ decodeConflict =
     Json.Decode.succeed Conflict
         |> Json.Decode.Extra.andMap (Json.Decode.field "EVENT_ID_NO_CNTY" Json.Decode.int)
         |> Json.Decode.Extra.andMap (Json.Decode.field "YEAR" Json.Decode.int)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "EVENT_DATE" Json.Decode.string)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "EVENT_TYPE" Json.Decode.string)
         |> Json.Decode.Extra.andMap (Json.Decode.field "FATALITIES" Json.Decode.int)
         |> Json.Decode.Extra.andMap (Json.Decode.field "NOTES" Json.Decode.string)
         |> Json.Decode.Extra.andMap (Json.Decode.field "COUNTRY" Json.Decode.string)
+        |> Json.Decode.Extra.andMap (Json.Decode.field "REGION" Json.Decode.string)
